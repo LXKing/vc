a94e1fcdce4e668cc1e7b49ebd59cdd8b5d4ebef
niuge <niuge@ccclubs.com>
2017-09-21 11:37:22 +0800

    新增报表统计功能

--- vc-web/vc-web-quota-api/src/main/java/com/ccclubs/quota/api/util/CsIndexReportUtil.java
+++ vc-web/vc-web-quota-api/src/main/java/com/ccclubs/quota/api/util/CsIndexReportUtil.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.math.BigDecimal;
 import java.util.*;
 
 /**
@@ -62,24 +63,14 @@ public class CsIndexReportUtil {
         String[] headersExit ={"VIN码","车机号","月均行驶里程(km)","平均单日运行时间(h)","百公里耗电量(kw/100km)","车辆纯电续驶里程(km)"
                 ,"最大充电功率(kw/h)","车辆一次充满电所用最少时间(h)","累计行驶里程(km)","累计充电量(kw)"};
         vinList.remove(0);
-        for (CsIndexReport csIndexReport:vinList){
-            if (existDateMap.containsKey(csIndexReport.getCsVin())){
-                CsIndexReport tempCsIndexReport=existDateMap.get(csIndexReport.getCsVin());
-                //
-                csIndexReport.setCsNumber(tempCsIndexReport.getCsNumber());
-                csIndexReport.setMonthlyAvgMile(tempCsIndexReport.getMonthlyAvgMile());
-                csIndexReport.setAvgDriveTimePerDay(tempCsIndexReport.getAvgDriveTimePerDay());
-                csIndexReport.setPowerConsumePerHundred(tempCsIndexReport.getPowerConsumePerHundred());
-                csIndexReport.setElectricRange(tempCsIndexReport.getElectricRange());
-                csIndexReport.setMaxChargePower(tempCsIndexReport.getMaxChargePower());
-                csIndexReport.setMinChargeTime(tempCsIndexReport.getMinChargeTime());
-                csIndexReport.setCumulativeMileage(tempCsIndexReport.getCumulativeMileage());
-                csIndexReport.setCumulativeCharge(tempCsIndexReport.getCumulativeCharge());
-            }
-        }
+        //正常数据
+//       Map<String,List<CsIndexReport>> dateMap=getZtNormData( existDateMap, vinList);
+
+         Map<String,List<CsIndexReport>> dateMap=getZtExceptionData( existDateMap, vinList);
         //
-        Map<String,List<CsIndexReport>>dateMap=new HashMap<>();
-        dateMap.put("查询结果",vinList);
+
+        //
+
         //
         ExportExcelTemp eeu = new ExportExcelTemp();
         HSSFWorkbook workbook=eeu.getWorkbook();
@@ -112,6 +103,140 @@ public class CsIndexReportUtil {
         }
         return   outPutByte;
     }
+
+    public static Map<String,List<CsIndexReport>> getZtNormData(Map<String,CsIndexReport> existDateMap,List<CsIndexReport> vinList){
+        for (CsIndexReport csIndexReport:vinList){
+            if (existDateMap.containsKey(csIndexReport.getCsVin())){
+                CsIndexReport tempCsIndexReport=existDateMap.get(csIndexReport.getCsVin());
+                //
+                csIndexReport.setCsNumber(tempCsIndexReport.getCsNumber());
+                csIndexReport.setMonthlyAvgMile(tempCsIndexReport.getMonthlyAvgMile());
+                csIndexReport.setAvgDriveTimePerDay(tempCsIndexReport.getAvgDriveTimePerDay());
+                csIndexReport.setPowerConsumePerHundred(tempCsIndexReport.getPowerConsumePerHundred());
+                csIndexReport.setElectricRange(tempCsIndexReport.getElectricRange());
+                csIndexReport.setMaxChargePower(tempCsIndexReport.getMaxChargePower());
+                csIndexReport.setMinChargeTime(tempCsIndexReport.getMinChargeTime());
+                csIndexReport.setCumulativeMileage(tempCsIndexReport.getCumulativeMileage());
+                csIndexReport.setCumulativeCharge(tempCsIndexReport.getCumulativeCharge());
+            }
+        }
+        Map<String,List<CsIndexReport>>dateMap=new HashMap<>();
+        dateMap.put("查询结果",vinList);
+        //
+        return dateMap;
+    }
+
+
+
+
+
+
+    public static Map<String,List<CsIndexReport>> getZtExceptionData(Map<String,CsIndexReport> existDateMap,List<CsIndexReport> vinList){
+
+        //异常筛选
+        //8<百公里耗电量<25
+        List<CsIndexReport>  powerConsumptionPerHundredList=new ArrayList<>();
+
+        //120<纯电续航里程<200
+        List<CsIndexReport>  pureElectricMileageList=new ArrayList<>();
+
+        //if累计行驶里程>0,then 月均行驶里程>0，平均单日运行时间>0,累计充电量>0
+        List<CsIndexReport>  cumulativeMileageList=new ArrayList<>();
+
+        //4.--2<最大充电功率<36
+        List<CsIndexReport>  maximumChargingPowerList=new ArrayList<>();
+
+        //5、一次充满电最少时间>0.8
+        List<CsIndexReport>  minimumFullTimeList=new ArrayList<>();
+
+
+
+
+        for (CsIndexReport csIndexReport:vinList){
+            if (existDateMap.containsKey(csIndexReport.getCsVin())){
+                CsIndexReport tempCsIndexReport=existDateMap.get(csIndexReport.getCsVin());
+                //
+                csIndexReport.setCsNumber(tempCsIndexReport.getCsNumber());
+                csIndexReport.setMonthlyAvgMile(tempCsIndexReport.getMonthlyAvgMile());
+                csIndexReport.setAvgDriveTimePerDay(tempCsIndexReport.getAvgDriveTimePerDay());
+                csIndexReport.setPowerConsumePerHundred(tempCsIndexReport.getPowerConsumePerHundred());
+                csIndexReport.setElectricRange(tempCsIndexReport.getElectricRange());
+                csIndexReport.setMaxChargePower(tempCsIndexReport.getMaxChargePower());
+                csIndexReport.setMinChargeTime(tempCsIndexReport.getMinChargeTime());
+                csIndexReport.setCumulativeMileage(tempCsIndexReport.getCumulativeMileage());
+                csIndexReport.setCurrentCumulativeMileage(tempCsIndexReport.getCurrentCumulativeMileage());
+                csIndexReport.setCumulativeCharge(tempCsIndexReport.getCumulativeCharge());
+
+                //1.百公里耗电量
+                BigDecimal powerConsumePerHundred= tempCsIndexReport.getPowerConsumePerHundred();
+                if(powerConsumePerHundred.intValue()>8&&powerConsumePerHundred.intValue()<25){
+                    powerConsumptionPerHundredList.add(csIndexReport);
+                }
+
+                //2、120<纯电续航里程<200
+                BigDecimal electricRange= tempCsIndexReport.getElectricRange();
+                if(electricRange.intValue()>120&&electricRange.intValue()<200){
+                    pureElectricMileageList.add(csIndexReport);
+                }
+
+
+
+
+                //3、if累计行驶里程>0,then 月均行驶里程>0，平均单日运行时间>0,累计充电量>0
+
+
+                BigDecimal cumulativeMileage= tempCsIndexReport.getCumulativeMileage();
+                BigDecimal monthlyAvgMile= tempCsIndexReport.getMonthlyAvgMile();
+                BigDecimal avgDriveTimePerDay = tempCsIndexReport.getAvgDriveTimePerDay();
+                BigDecimal cumulativeCharge=tempCsIndexReport.getCumulativeCharge();
+
+                if(cumulativeMileage.intValue()>8&&monthlyAvgMile.intValue()>0
+                        &&monthlyAvgMile.intValue()>0&&cumulativeCharge.intValue()>0){
+                    cumulativeMileageList.add(csIndexReport);
+                }
+
+                //4、2<最大充电功率<36
+                BigDecimal maxChargePower= tempCsIndexReport.getMaxChargePower();
+                if(maxChargePower.intValue()>2&&maxChargePower.intValue()<36){
+                    maximumChargingPowerList.add(csIndexReport);
+                }
+
+                //5、一次充满电最少时间>0.8
+                BigDecimal minChargeTime= tempCsIndexReport.getMinChargeTime();
+                if(minChargeTime.intValue()>0.8){
+                    minimumFullTimeList.add(csIndexReport);
+                }
+
+
+            }
+        }
+
+        Map<String,List<CsIndexReport>>dateMap=new HashMap<>();
+        dateMap.put("查询结果",vinList);
+        //
+        dateMap.put("8<百公里耗电量<25",powerConsumptionPerHundredList);
+        dateMap.put("120<纯电续航里程<200",pureElectricMileageList);
+        dateMap.put("if累计行驶里程>0,then月均行驶里程>0,平均单日运行时间>0,累计充电量>0",cumulativeMileageList);
+        dateMap.put("2<最大充电功率<36",maximumChargingPowerList);
+        dateMap.put("一次充满电最少时间>0.8",minimumFullTimeList);
+
+
+        return dateMap;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     @Scheduled(cron="0 15 3 * * ?")
     public void clearExcelBinaryMap(){
