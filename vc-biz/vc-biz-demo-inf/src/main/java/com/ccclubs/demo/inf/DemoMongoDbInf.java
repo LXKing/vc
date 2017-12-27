c33305a7a1b3d8e7da1f1585a4509922dc59a9a8
Alban <mrfj@live.cn>
2017-12-27 11:32:02 +0800

    清理无用模块。

--- vc-biz/vc-biz-demo-inf/src/main/java/com/ccclubs/demo/inf/DemoMongoDbInf.java
+++
@@ -1,7 +0,0 @@
-package com.ccclubs.demo.inf;
-
-import com.ccclubs.demo.mod.DemoEntity;
-
-public interface DemoMongoDbInf {
-	String sayMongoDemo(DemoEntity mongo);
-}
--- vc-biz/vc-biz-demo-srv/src/main/java/com/ccclubs/demo/mono/dao/DemoMongoDbDao.java
+++
@@ -1,8 +0,0 @@
-package com.ccclubs.demo.mono.dao;
-
-import com.ccclubs.demo.mod.DemoEntity;
-
-public interface DemoMongoDbDao {
-	void saveDemo(DemoEntity demo);
-	DemoEntity findDemoByMyName(String myName);
-}
