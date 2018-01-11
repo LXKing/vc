b5a09e1bed65f1c36b3a921f331700125fa2c8aa
Alban <mrfj@live.cn>
2018-01-11 15:54:34 +0800

    no message

--- vc-web/vc-web-front/src/main/java/com/ccclubs/admin/service/impl/HistoryGbServiceImpl.java
+++ vc-web/vc-web-front/src/main/java/com/ccclubs/admin/service/impl/HistoryGbServiceImpl.java
@@ -86,7 +86,7 @@ public class HistoryGbServiceImpl implements IHistoryGbService{
             historyGb.setCsProtocol(carGb.getCs_protocol());
             historyGb.setCsVerify(carGb.getCs_verify());
             historyGb.setCsVin(carGb.getCs_vin());
-            if (null!=carGb.getCurrent_time()) {
+            if (null!=carGb.getCurrent_time()&&carGb.getCurrent_time()!=0L) {
                 historyGb.setCurrentTime(new Date(carGb.getCurrent_time()));
             }
             historyGb.setGbData(carGb.getGb_data());
