66310835e1459c04b04d81715fd6780aaeb59fee
Alban <mrfj@live.cn>
2018-01-23 10:43:08 +0800

    添加can数据导出。

---
+++ vc-biz/vc-biz-message/vc-biz-gateway/vc-biz-gateway-zotye-inf/src/main/java/com/ccclubs/gateway/zotye/constant/ServiceListForRouting.java
@@ -0,0 +1,22 @@
+package com.ccclubs.gateway.zotye.constant;
+
+/**
+ * \* Created with IntelliJ IDEA2017.
+ * \* User: Alban
+ * \* Date: 2018/1/19
+ * \* Time: 15:40
+ * \* Description:
+ * \
+ */
+public enum ServiceListForRouting {
+    REGISTER_BIND(),
+    LOGIN(),
+    LOGOUT(),
+    RELOGIN(),
+    GET_MESSAGE_HEAREBEAT(),
+    READ_CONFIG(),
+    ISSUED_CONFIG(),
+    UPDATEVERSION(),
+    COMMUNICATION_MAINTAINED(),
+
+}
