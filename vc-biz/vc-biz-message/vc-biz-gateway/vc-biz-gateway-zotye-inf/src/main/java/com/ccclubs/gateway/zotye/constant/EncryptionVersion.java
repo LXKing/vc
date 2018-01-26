d388739926b88ff81416f988ae19589aa5a4d4c2
Alban <mrfj@live.cn>
2018-01-26 15:03:15 +0800

    还原规则引擎检查队列大小模块

--- vc-biz/vc-biz-message/vc-biz-gateway/vc-biz-gateway-zotye-inf/src/main/java/com/ccclubs/gateway/zotye/constant/EncryptionVersion.java
+++ vc-biz/vc-biz-message/vc-biz-gateway/vc-biz-gateway-zotye-inf/src/main/java/com/ccclubs/gateway/zotye/constant/EncryptionVersion.java
@@ -4,9 +4,13 @@ import java.io.Serializable;
 
 public enum EncryptionVersion implements Serializable {
 
+    //不加密
     NONE(0x0),
+    //AES128BIT加密
     AES128BIT(0x1),
+    //AES256BIT加密
     AES256BIT(0x2),
+    //BASE64加密
     BASE64(0x3);
 
     private Integer bitValue;
