f0f804827d814d1183aaf0fdf75c4da61f017c9e
江海洋 <jianghaiyang@ccclubs.com>
2018-03-05 21:20:43 +0800

    修改定时

--- vc-web/vc-web-front/src/main/java/com/ccclubs/admin/task/schedulers/StatisticsScheduler.java
+++ vc-web/vc-web-front/src/main/java/com/ccclubs/admin/task/schedulers/StatisticsScheduler.java
@@ -58,7 +58,7 @@ public class StatisticsScheduler implements ApplicationContextAware {
     @Resource
     ExpDataCheckJob expDataCheckJob;
 
-    @Scheduled(fixedRate = 200000)
+    @Scheduled(cron="0 0 1,19 * * ?")
     public void expDataCheckJob(){
         logger.info("执行了一次 车辆异常数据的巡检。");
         EvManageContext.getThreadPool().execute(expDataCheckJob);
