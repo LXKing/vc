23fe30406d1220b4c7fb5a1262e63f30a77a8e4a
徐刚 <xugang@ccclubs.com>
2018-03-06 18:58:36 +0800

    V10车型，2017年生产的车辆，累计里程在充电时，存在跳变，实时数据为0时，平台需要矫正，加入最后一次最新里程

--- vc-biz/vc-biz-protocol-inf/src/main/java/com/ccclubs/protocol/dto/gb/GBMessageType.java
+++ vc-biz/vc-biz-protocol-inf/src/main/java/com/ccclubs/protocol/dto/gb/GBMessageType.java
@@ -32,4 +32,41 @@ public class GBMessageType {
      */
     public static final int GB_MSG_TYPE_0X06 = 0x06;
 
+    /**
+     * 实时信息：0x01:整车数据
+     */
+    public static final int GB_MSG_REAL_TIME_TYPE_0X01 = 0x01;
+    /**
+     * 实时信息：0x02:驱动电机数据
+     */
+    public static final int GB_MSG_REAL_TIME_TYPE_0X02 = 0x02;
+    /**
+     * 实时信息：0x03:燃料电池数据
+     */
+    public static final int GB_MSG_REAL_TIME_TYPE_0X03 = 0x03;
+      /**
+     * 实时信息：0x04:发动机数据
+     */
+    public static final int GB_MSG_REAL_TIME_TYPE_0X04 = 0x04;
+      /**
+     * 实时信息：0x05:车辆位置数据
+     */
+    public static final int GB_MSG_REAL_TIME_TYPE_0X05 = 0x05;
+      /**
+     * 实时信息：0x06:极值数据
+     */
+    public static final int GB_MSG_REAL_TIME_TYPE_0X06 = 0x06;
+      /**
+     * 实时信息：0x07:报警数据
+     */
+    public static final int GB_MSG_REAL_TIME_TYPE_0X07 = 0x07;
+      /**
+     * 实时信息：0x08:可充电储能装置电压数据
+     */
+    public static final int GB_MSG_REAL_TIME_TYPE_0X08 = 0x08;
+      /**
+     * 实时信息：0x09:可充电储能装置温度数据
+     */
+    public static final int GB_MSG_REAL_TIME_TYPE_0X09 = 0x09;
+
 }
