b427d1cbd0e7fd512b8e878033b1f8ef1ba6d988
江海洋 <jianghaiyang@ccclubs.com>
2018-03-06 15:48:54 +0800

    增加手动触发接口

--- vc-web/vc-web-front/src/main/java/com/ccclubs/admin/config/MvcConfigurer.java
+++ vc-web/vc-web-front/src/main/java/com/ccclubs/admin/config/MvcConfigurer.java
@@ -36,6 +36,7 @@ public class MvcConfigurer extends WebMvcConfigurerAdapter {
             //.excludePathPatterns("/oauth/**")
             .excludePathPatterns("/user/**")
             .excludePathPatterns("/monitor/gbState/set")
+            .excludePathPatterns("/monitor/expVehicle/trigger")
             .excludePathPatterns("/protocol/**")
             .excludePathPatterns("/monitor/gbState/detail/**")
             .excludePathPatterns("/report/**");
