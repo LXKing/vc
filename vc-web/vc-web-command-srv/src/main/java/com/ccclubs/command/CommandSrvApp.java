8bdf427fc788de1ffb28d5b06be56ff616bd34e5
江海洋 <jianghaiyang@ccclubs.com>
2018-03-07 20:05:21 +0800

    线程池修改

--- vc-web/vc-web-command-srv/src/main/java/com/ccclubs/command/CommandSrvApp.java
+++ vc-web/vc-web-command-srv/src/main/java/com/ccclubs/command/CommandSrvApp.java
@@ -8,6 +8,7 @@ import com.ccclubs.frm.mqtt.inf.IMqClient;
 import com.ccclubs.frm.mqtt.inf.impl.MqMqttClient;
 import com.ccclubs.frm.mybatis.MybatisConfig;
 import com.ccclubs.frm.redis.RedisAutoConfiguration;
+
 import java.io.IOException;
 import java.util.concurrent.*;
 
@@ -15,6 +16,7 @@ import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -29,66 +31,81 @@ import org.springframework.context.annotation.Primary;
 @SpringBootApplication
 @Import({MybatisConfig.class})
 @EnableConfigurationProperties({CommandProp.class, MqttAliyunProperties.class,
-    MqttOwnProperties.class})
+        MqttOwnProperties.class})
 @ImportAutoConfiguration({RedisAutoConfiguration.class, MultipleMongoConfig.class})
 public class CommandSrvApp extends SpringBootServletInitializer {
 
-  private static final Logger logger = LoggerFactory.getLogger(CommandSrvApp.class);
+    private static final Logger logger = LoggerFactory.getLogger(CommandSrvApp.class);
 
-  @Autowired
-  private MqttAliyunProperties mqttAliyunProperties;
-  @Autowired
-  private MqttOwnProperties mqttOwnProperties;
+    @Autowired
+    private MqttAliyunProperties mqttAliyunProperties;
+    @Autowired
+    private MqttOwnProperties mqttOwnProperties;
 
-  /**
-   * war打包用，相当于web.xml配置
-   */
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-    return application.sources(CommandSrvApp.class);
-  }
-
-  public static void main(String[] args) throws IOException, InterruptedException {
-    ConfigurableApplicationContext ctx = SpringApplication.run(CommandSrvApp.class, args);
-    String[] profiles = ctx.getEnvironment().getActiveProfiles();
-    for (String p : profiles) {
-      logger.info("Env profile:{}", p);
+    /**
+     * war打包用，相当于web.xml配置
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CommandSrvApp.class);
     }
-  }
 
-  @Bean(name = "aliyunMqttClient", initMethod = "start", destroyMethod = "stop")
-  @Primary
-  IMqClient getAliyunMqttClient() {
-    MqMqttClient mqClient = new MqMqttClient();
-    mqClient.setHost(mqttAliyunProperties.getHost());
-    mqClient.setListenPort(mqttAliyunProperties.getPort());
-    mqClient.setClientIdPre(mqttAliyunProperties.getPreId());
-    mqClient.setUserName(mqttAliyunProperties.getUserName());
-    mqClient.setPwd(mqttAliyunProperties.getPwd());
-    mqClient.setLogUpDown(mqttAliyunProperties.isLogUpDown());
-    mqClient.setSubTopic(mqttAliyunProperties.getSubTopic());
-    return mqClient;
-  }
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ConfigurableApplicationContext ctx = SpringApplication.run(CommandSrvApp.class, args);
+        String[] profiles = ctx.getEnvironment().getActiveProfiles();
+        for (String p : profiles) {
+            logger.info("Env profile:{}", p);
+        }
+    }
 
-  @Bean(name = "ownMqttClient", initMethod = "start", destroyMethod = "stop")
-  IMqClient getOwnMqttClient() {
-    MqMqttClient ownMqClient = new MqMqttClient();
-    ownMqClient.setHost(mqttOwnProperties.getHost());
-    ownMqClient.setListenPort(mqttOwnProperties.getPort());
-    ownMqClient.setClientIdPre(mqttOwnProperties.getPreId());
-    ownMqClient.setUserName(mqttOwnProperties.getUserName());
-    ownMqClient.setPwd(mqttOwnProperties.getPwd());
-    ownMqClient.setLogUpDown(mqttOwnProperties.isLogUpDown());
-    ownMqClient.setSubTopic(mqttOwnProperties.getSubTopic());
-    return ownMqClient;
-  }
+    @Bean(name = "aliyunMqttClient", initMethod = "start", destroyMethod = "stop")
+    @Primary
+    IMqClient getAliyunMqttClient() {
+        MqMqttClient mqClient = new MqMqttClient();
+        mqClient.setHost(mqttAliyunProperties.getHost());
+        mqClient.setListenPort(mqttAliyunProperties.getPort());
+        mqClient.setClientIdPre(mqttAliyunProperties.getPreId());
+        mqClient.setUserName(mqttAliyunProperties.getUserName());
+        mqClient.setPwd(mqttAliyunProperties.getPwd());
+        mqClient.setLogUpDown(mqttAliyunProperties.isLogUpDown());
+        mqClient.setSubTopic(mqttAliyunProperties.getSubTopic());
+        return mqClient;
+    }
 
-  @Bean("vcThreadPool")
-  public ExecutorService getThreadPool() {
-    ThreadFactory threadFactory = new ThreadFactoryBuilder().setNameFormat("remote-pool-%d").build();
-    ExecutorService pool = new ThreadPoolExecutor(80, 200, 60L, TimeUnit.SECONDS
-            , new LinkedBlockingDeque<>(2048), threadFactory, new ThreadPoolExecutor.AbortPolicy());
-    return pool;
-  }
+    @Bean(name = "ownMqttClient", initMethod = "start", destroyMethod = "stop")
+    IMqClient getOwnMqttClient() {
+        MqMqttClient ownMqClient = new MqMqttClient();
+        ownMqClient.setHost(mqttOwnProperties.getHost());
+        ownMqClient.setListenPort(mqttOwnProperties.getPort());
+        ownMqClient.setClientIdPre(mqttOwnProperties.getPreId());
+        ownMqClient.setUserName(mqttOwnProperties.getUserName());
+        ownMqClient.setPwd(mqttOwnProperties.getPwd());
+        ownMqClient.setLogUpDown(mqttOwnProperties.isLogUpDown());
+        ownMqClient.setSubTopic(mqttOwnProperties.getSubTopic());
+        return ownMqClient;
+    }
+
+    @Value("${ThreadPool.nameFormat}")
+    String nameFormat;
+
+    @Value("${ThreadPool.corePoolSize}")
+    int corePoolSize;
+
+    @Value("${ThreadPool.maximumPoolSize}")
+    int maximumPoolSize;
+
+    @Value("${ThreadPool.keepAliveTime}")
+    long keepAliveTime;
+
+    @Value("${ThreadPool.dequeCapacity}")
+    int dequeCapacity;
+
+    @Bean("vcThreadPool")
+    public ExecutorService getThreadPool() {
+        ThreadFactory threadFactory = new ThreadFactoryBuilder().setNameFormat(nameFormat).build();
+        ExecutorService pool = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.SECONDS
+                , new LinkedBlockingDeque<>(dequeCapacity), threadFactory, new ThreadPoolExecutor.AbortPolicy());
+        return pool;
+    }
 
 }
