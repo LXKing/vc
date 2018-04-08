94c95025f6bdb56888fb1c5673425bb86fbedc74
Alban <mrfj@live.cn>
2018-04-08 14:19:12 +0800

    no message

---
+++ vc-biz/vc-biz-terminal-inf/src/main/java/com/ccclubs/terminal/dto/VehicleStatesQryInput.java
@@ -0,0 +1,13 @@
+package com.ccclubs.terminal.dto;
+
+/**
+ * Created with IntelliJ IDEA2017.
+ *
+ * @Author: Alban
+ * @Date: 2018/4/8
+ * @Time: 10:21
+ * @Description: 请填写描述！
+ */
+
+public class VehicleStatesQryInput {
+}
