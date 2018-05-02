073b1e8a859edbda3ac71da5995bad8c739584a0
江海洋 <jianghaiyang@ccclubs.com>
2018-05-02 17:07:59 +0800

    测试kafka SLB

--- vc-biz/vc-biz-storage-srv/src/main/java/com/ccclubs/storage/consumer/CsStateConsumer.java
+++ vc-biz/vc-biz-storage-srv/src/main/java/com/ccclubs/storage/consumer/CsStateConsumer.java
@@ -29,7 +29,7 @@ public class CsStateConsumer {
 
     @Autowired
     HistoryStateUtils historyStateUtils;
-    @KafkaListener(id = "${" + KAFKA_CONSUMER_GROUP_STORAGE_STATE + "}", topics = "${" + KAFKA_TOPIC_CS_STATE + "}", containerFactory = "batchFactory")
+    //@KafkaListener(id = "${" + KAFKA_CONSUMER_GROUP_STORAGE_STATE + "}", topics = "${" + KAFKA_TOPIC_CS_STATE + "}", containerFactory = "batchFactory")
     public void processCanStorage(List<String> messageList){
         List<CsState> csStateList=new ArrayList<>();
         for (String message:messageList){
