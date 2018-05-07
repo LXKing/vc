9baf6ac6b037d9370087e197fe6d5f5b48a1b8c3
guojun <guojun@ccclubs.com>
2018-05-07 20:12:56 +0800

    拆分url

--- vc-web/vc-web-front/src/main/resources/static/js/tpui-core.js
+++ vc-web/vc-web-front/src/main/resources/static/js/tpui-core.js
@@ -1,13 +1,8 @@
-/**
- * 应用服务地址
- */
-// var servUrl = "http://118.178.230.105:7004";
-var servUrl 	= "http://localhost:9009";
-// var servUrl     = "http://localhost:8080"
-/**
- * 统一认证地址
- */
-var authUrl = "http://localhost:9009";
+var newscript = document.createElement('script');  
+newscript.setAttribute('type','text/javascript');  
+newscript.setAttribute('src','/js/url.js');  
+var head = document.getElementsByTagName('head')[0];  
+head.appendChild(newscript);
 
 function getServUrl(path) {
     // if (path.indexOf("crback/") != -1) {
