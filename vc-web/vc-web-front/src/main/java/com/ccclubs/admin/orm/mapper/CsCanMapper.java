cef8c7235caade8379b0ce4325d2fa8a70eed996
machuanpeng <machuanpeng1991@163.com>
2018-09-18 13:44:55 +0800

    【Add】添加首页车辆统计

--- vc-web/vc-web-front/src/main/java/com/ccclubs/admin/orm/mapper/CsCanMapper.java
+++ vc-web/vc-web-front/src/main/java/com/ccclubs/admin/orm/mapper/CsCanMapper.java
@@ -1,12 +1,21 @@
 package com.ccclubs.admin.orm.mapper;
 
-import com.ccclubs.frm.base.BaseDAO;
 import com.ccclubs.admin.model.CsCan;
+import com.ccclubs.frm.base.BaseDAO;
 
 /**
  * 车辆实时CAN信息的Mapper实现
+ *
  * @author Joel
  */
-public interface CsCanMapper extends BaseDAO<CsCan,Long>{
-	
+public interface CsCanMapper extends BaseDAO<CsCan, Long> {
+    /**
+     * 2018/9/18
+     * 车辆数 state=null:全部，0不在线，1在线
+     *
+     * @param state
+     * @return java.lang.Integer
+     * @author machuanpeng
+     */
+    Integer getCarCount(Integer state);
 }
--- vc-web/vc-web-front/src/main/java/com/ccclubs/admin/service/ICsCanService.java
+++ vc-web/vc-web-front/src/main/java/com/ccclubs/admin/service/ICsCanService.java
@@ -5,8 +5,17 @@ import com.ccclubs.admin.model.CsCan;
 
 /**
  * 车辆实时CAN信息的Service接口
+ *
  * @author Joel
  */
-public interface ICsCanService extends BaseService<CsCan, Long>{
-	
+public interface ICsCanService extends BaseService<CsCan, Long> {
+    /**
+     * 2018/9/18
+     * 车辆数 state=null:全部，0不在线，1在线
+     *
+     * @param state
+     * @return java.lang.Integer
+     * @author machuanpeng
+     */
+    Integer getCarCount(Integer state);
 }
